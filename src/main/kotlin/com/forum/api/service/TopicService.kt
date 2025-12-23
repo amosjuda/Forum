e5162e944d19069e3ef3d51f144d17416bcf9e93
amosjuda <amosjudasilva@gmail.com>
2025-12-23 16:51:40 -0300

    refactor: migrate TopicService to use DTOs and Mappers

--- src/main/kotlin/com/forum/api/service/TopicService.kt
+++ src/main/kotlin/com/forum/api/service/TopicService.kt
@@ -1,71 +1,57 @@
 package com.forum.api.service
 
-import com.forum.api.model.Author
-import com.forum.api.model.Course
+import com.forum.api.dto.NewTopicForm
+import com.forum.api.dto.TopicView
+import com.forum.api.dto.UpdateTopicForm
+import com.forum.api.exception.NotFoundException
+import com.forum.api.mapper.TopicFormMapper
+import com.forum.api.mapper.TopicViewMapper
 import com.forum.api.model.Topic
 import org.springframework.stereotype.Service
-import java.util.Arrays
 
 @Service
 class TopicService(
-    private var topics: List<Topic>
+        private var topics: List<Topic> = ArrayList(),
+        private val topicViewMapper: TopicViewMapper,
+        private val topicFormMapper: TopicFormMapper
 ) {
-    init {
-        val topic1 = Topic(
-            id = 1,
-            title = "Kotlin doubt",
-            description = "Kotlin variable",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "programming"
-            ),
-            author = Author(
-                id = 1,
-                name = "Ana da silva",
-                email = "ana@gmail.com"
-            )
-        )
-        val topic2 = Topic(
-            id = 2,
-            title = "Kotlin doubt",
-            description = "Kotlin variable",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "programming"
-            ),
-            author = Author(
-                id = 1,
-                name = "Ana da silva",
-                email = "ana@gmail.com"
-            )
-        )
-        val topic3 = Topic(
-            id = 3,
-            title = "Kotlin doubt",
-            description = "Kotlin variable",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "programming"
-            ),
-            author = Author(
-                id = 1,
-                name = "Ana da silva",
-                email = "ana@gmail.com"
-            )
-        )
-        topics = Arrays.asList(topic1, topic2, topic3)
+
+    fun list(): List<TopicView> =
+        topics.map { topic -> topicViewMapper.map(topic) }
+
+    fun searchById(id: Long): TopicView {
+        val topic = topics.firstOrNull { t -> t.id == id }
+            ?: throw NotFoundException("Topic not found")
+        return topicViewMapper.map(topic)
     }
 
-    fun list(): List<Topic> {
-        return topics
+    fun register(form: NewTopicForm): TopicView {
+        val topic = topicFormMapper.map(form)
+        topic.id = topics.size.toLong() + 1
+        topics = topics.plus(topic)
+        return topicViewMapper.map(topic)
     }
 
-    fun searchById(id: Long): Topic {
-        return topics.stream().filter({
-            t -> t.id == id
-        }).findFirst().get()
+    fun update(form: UpdateTopicForm): TopicView {
+        val topic = topics.firstOrNull { t -> t.id == form.id }
+            ?: throw NotFoundException("Topic not found")
+        val updatedTopic = Topic(
+            id = form.id,
+            title = form.title,
+            message = form.message,
+            author = topic.author,
+            course = topic.course,
+            status = topic.status,
+            answers = topic.answers,
+            createTime = topic.createTime,
+        )
+        topics = topics.minus(topic).plus(updatedTopic)
+        return topicViewMapper.map(updatedTopic)
+    }
+
+    fun delete(id: Long) {
+        val topic = topics.firstOrNull { t -> t.id == id }
+            ?: throw NotFoundException("Topic not found")
+        topics = topics.minus(topic)
     }
 }
