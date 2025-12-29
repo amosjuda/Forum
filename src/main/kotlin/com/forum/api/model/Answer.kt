f3050902f2031f053a4eef2d0bf1370d64d9d28c
amosjuda <amosjudasilva@gmail.com>
2025-12-29 16:55:56 -0300

    feat(entitys): Mapped entities

--- src/main/kotlin/com/forum/api/model/Answer.kt
+++ src/main/kotlin/com/forum/api/model/Answer.kt
@@ -2,7 +2,6 @@ package com.forum.api.model
 
 import com.forum.api.model.enums.StatusTopic
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
--- src/main/kotlin/com/forum/api/model/Topic.kt
+++ src/main/kotlin/com/forum/api/model/Topic.kt
@@ -1,15 +1,30 @@
 package com.forum.api.model
 
 import com.forum.api.model.enums.StatusTopic
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import java.time.LocalDateTime
 
+@Entity
 data class Topic (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val title: String,
     val message: String,
     val createTime: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne(fetch = FetchType.LAZY)
     val course: Course,
+    @ManyToOne(fetch = FetchType.LAZY)
     val author: User,
+    @Enumerated(EnumType.STRING)
     val status: StatusTopic = StatusTopic.NOT_ANSWERED,
-    val answers: List<Answers> = ArrayList(),
+    @OneToMany(mappedBy = "topic")
+    val answers: List<Answer> = ArrayList(),
 )
