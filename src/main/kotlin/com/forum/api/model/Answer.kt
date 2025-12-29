314707717424336c87373ef4243833b1f9806894
amosjuda <amosjudasilva@gmail.com>
2025-12-29 16:40:48 -0300

    feat(refactor): renaming Answers to Answers and application.properties to application.yml

---
+++ src/main/kotlin/com/forum/api/model/Answer.kt
@@ -0,0 +1,23 @@
+package com.forum.api.model
+
+import com.forum.api.model.enums.StatusTopic
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+
+@Entity
+data class Answer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    val message: String,
+    val creationDate: String,
+    @ManyToOne
+    val course: Course,
+    @ManyToOne
+    val author: User,
+    val status: StatusTopic = StatusTopic.NOT_ANSWERED,
+    val answers: List<Answer> = ArrayList(),
+)
